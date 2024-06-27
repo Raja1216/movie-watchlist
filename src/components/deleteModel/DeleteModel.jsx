9ccbff5333e88692337709d9b853dbc19cf9a514
Subhajit <subha.pramanik1212@gmail.com>
2024-06-27 23:38:40 +0530

    first commit1

--- src/components/deleteModel/DeleteModel.jsx
+++ src/components/deleteModel/DeleteModel.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { removeMovie } from "../../features/movies/moviesSlice";
 import { useNavigate } from "react-router-dom";
 
+
 Modal.setAppElement("#root");
 
 const DeleteModal = ({ isOpen, onRequestClose, movie }) => {
